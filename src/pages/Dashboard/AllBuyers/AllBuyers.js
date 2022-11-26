5cdde3d9578a862dacdffed00a6be6bba35a6dee
rahiislam96 <nazrulislam66188@gmail.com>
2022-11-26 17:59:58 +0600

    all buyer added

--- src/pages/Dashboard/AllBuyers/AllBuyers.js
+++ src/pages/Dashboard/AllBuyers/AllBuyers.js
@@ -1,9 +1,46 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const AllBuyers = () => {
+
+    const { data: buyers = [] } = useQuery({
+        queryKey: ['buyers'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/users/all-buyers');
+            const data = await res.json();
+            return data;
+        }
+    })
+
     return (
         <div>
-            <p>This is all buyers</p>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Activity</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            buyers.map((buyer, i) =>
+                                <tr key={buyer._id} className="hover">
+                                    <th>{i + 1}</th>
+                                    <td>{buyer.displayName}</td>
+                                    <td>{buyer.email}</td>
+                                    <td>
+                                        <button className='btn btn-error btn-xs'>Delete</button>
+                                    </td>
+                                </tr>
+                            )
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
