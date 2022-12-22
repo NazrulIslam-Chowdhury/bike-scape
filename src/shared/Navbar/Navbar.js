2cc7ce63477f3d1bd39c5a6823713ece3a7f8a5d
rahiislam96 <nazrulislam66188@gmail.com>
2022-12-22 16:59:59 +0600

    logo updated

--- src/shared/Navbar/Navbar.js
+++ src/shared/Navbar/Navbar.js
@@ -80,7 +80,7 @@ const Navbar = () => {
                 </div>
 
                 <Link to='/' >
-                    <img src={logo} className='lg:w-44 md:w-44 lg:h-44 md:h-44 w-24 h-24' alt="" />
+                    <img src={logo} className='lg:w-44 md:w-44 lg:h-44 md:h-44 w-28 h-24' alt="" />
                 </Link>
                 <label htmlFor="dashboard-drawer" tabIndex={2} className="btn btn-ghost lg:hidden">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
